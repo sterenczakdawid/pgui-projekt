ecf5ba2221c98ea16aebbb5147cd5733efe2c4a0
Dawid <sterenczak.dawid@gmail.com>
2023-12-18 20:25:25 +0100

    changes

--- src/core/store/appSettingsSlice.ts
+++ src/core/store/appSettingsSlice.ts
@@ -1,13 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const THEME_KEY = "theme";
-export const LANGUAGE_KEY = "language";
-
 export const appSettingsSlice = createSlice({
 	name: "globalSettings",
 	initialState: {
-		language: window.localStorage.getItem(LANGUAGE_KEY) || "en",
-		theme: window.localStorage.getItem(THEME_KEY) || "light",
+		language: "en",
+		theme: "light",
 	},
 	reducers: {},
 });
