5434baec22ce006c0adff0586c80c7c8dc05fe14
Piotr Wiśniewski <piotrwisnie@wp.pl>
2023-12-28 20:33:05 +0100

    start of redux; basic theme change logic applied

--- src/core/store/appSettingsSlice.ts
+++ src/core/store/appSettingsSlice.ts
@@ -1,12 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+interface settingsState {
+	language: "en" | "pl";
+	theme: "light" | "dark";
+	// cos tam cos tam
+}
+
+const initialState: settingsState = {
+	language: "en",
+	theme: "light",
+}
+
+
 export const appSettingsSlice = createSlice({
 	name: "globalSettings",
-	initialState: {
-		language: "en",
-		theme: "light",
+	initialState,
+	reducers: {
+		LANG: (state) => {
+			if (state.language == "en") {
+				state.language = "pl";
+			} else {
+				state.language = "en";
+			}
+		},
+		changeTheme: (state) => {
+			if (state.theme == "light") {
+				state.theme = "dark";
+			} else {
+				state.theme = "light";
+			}
+		}
 	},
-	reducers: {},
 });
+
+export const { LANG, changeTheme } = appSettingsSlice.actions;
 
 export default appSettingsSlice.reducer;
--- src/core/store/store.ts
+++ src/core/store/store.ts
@@ -1,5 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+import settingsReducer from './appSettingsSlice';
 
-export default configureStore({
-	reducer: {},
-});
+// export default configureStore({
+// 	reducer: {},
+// });
+
+export const store = configureStore({
+	reducer: {
+		globalSettings: settingsReducer
+	},
+})
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
