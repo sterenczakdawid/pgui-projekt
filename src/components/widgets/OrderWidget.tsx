e3707f8f6d2f38ef42c316739e18346c354745cd
Pioter17 <piotrwisnie@wp.pl>
2024-01-17 19:08:35 +0100

    Sales quality widget done

--- src/components/widgets/OrderWidget.tsx
+++ src/components/widgets/OrderWidget.tsx
@@ -6,19 +6,21 @@ export const OrderWidget = () => {
 
 	return <div className="card">
 		<TitleLink title={"Zamówienia"} link={"/orders"}/>
-		{
-			tabs.map((res, idx)=>(
-				<div className="order__tab flex justify-between" key={idx}>
-					<span>{res.name}</span>
-					<div className="flex justify-evenly space-x-8">
-						<span className="font-bold text-xl">{res.quantity}</span>
-						<span className="material-symbols-outlined">
-							arrow_forward_ios
-						</span>
+		<div className="mt-10">
+			{
+				tabs.map((res, idx)=>(
+					<div className="order__tab flex justify-between" key={idx}>
+						<span>{res.name}</span>
+						<div className="flex justify-evenly space-x-8">
+							<span className="font-bold text-xl">{res.quantity}</span>
+							<span className="material-symbols-outlined">
+								arrow_forward_ios
+							</span>
+						</div>
+						
 					</div>
-					
-				</div>
-			))
-		}
+				))
+			}
+		</div>
 	</div>;
 };
--- src/components/widgets/SalesQualityWidget.tsx
+++ src/components/widgets/SalesQualityWidget.tsx
@@ -1,7 +1,34 @@
 import { TitleLink } from "../layout/TitleLink";
 
+const aspects = [{name: "Czas dostawy", rating: "4.0/5"}, {name: "Obsługa klienta", rating: "3.5/5"}, {name: "Jakość produktów", rating: "3.0/5"}];
+
 export const SalesQualityWidget = () => {
 	return <div className="card">
 		<TitleLink title={"Jakość sprzedaży"} link={"/sales-quality"}/>
+		<div className="flex p-2.5 flex-col">
+			<div className="flex justify-between mt-2 items-center space-x-8">
+				<span className="text-sm">Kategoria jakości</span>
+				<span className="quality__cat">C</span>
+			</div>
+			<div className="flex justify-between mt-2 items-center space-x-8">
+				<span className="text-sm">Ocena jakości</span>
+				<span className="font-bold">21/30</span>
+			</div>
+			<hr className="line" />
+			<span className="text-center mb-2">Najgorzej oceniane aspekty:</span>
+			{
+				aspects.map((res, idx)=>(
+					<div className="mt-3 flex justify-between" key={idx}>
+						<span className="w-40">{res.name}</span>
+						<div className="outer-bar">
+							<div className="inner-bar"></div>
+						</div>
+						<div className="flex justify-evenly space-x-8">
+							<span className="font-bold text-sm">{res.rating}</span>
+						</div>						
+					</div>
+				))
+			}
+		</div>
 	</div>;
 };
