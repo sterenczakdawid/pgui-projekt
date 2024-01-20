937436afaa552fa758b4ee9348cd5e470bc8d10e
Dawid <sterenczak.dawid@gmail.com>
2024-01-21 00:47:29 +0100

    top bar css fixes

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -2,31 +2,35 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../core/store/store";
 import { changeShop } from "../core/store/appSettingsSlice";
 
-
 export const TopBar = () => {
-	const user = useSelector((state:RootState) => state.globalSettings.user);
+	const user = useSelector((state: RootState) => state.globalSettings.user);
 	const ownedShops = user.ownedShops;
 	const dispatch = useDispatch();
 
 	const changeShopDisplayed = (event) => {
 		const selectedValue = event.target.value;
-		if (selectedValue !== ""){
-			dispatch(changeShop(+selectedValue))
+		if (selectedValue !== "") {
+			dispatch(changeShop(+selectedValue));
 		}
 	};
 
-	return <div className="topbar flex justify-end gap-4 border-b-2 border-b-[#5a57ff] z-[1]">
-		<select onChange={changeShopDisplayed} value={user.currentShop}>
-			<option value="">Wybierz opcję</option>
-			{ownedShops.map((shopId) => (
-			<option key={shopId} value={shopId}>
-				Sklep {shopId}
-			</option>
-			))}
-		</select>
-		<div className="mr-4">
-			Użytkownik: {user.name} <br />
-			sklep: {user.currentShop} <br />
+	return (
+		<div className="topbar flex justify-between items-center gap-4 border-b-2 border-b-[#5a57ff] z-[1]">
+			<select className="font-bold rounded-3xl h-[50px] border-[#5a57ff] ml-[260px]" onChange={changeShopDisplayed} value={user.currentShop}>
+				<option value="">Wybierz opcję</option>
+				{ownedShops.map((shopId) => (
+					<option key={shopId} value={shopId}>
+						Sklep {shopId}
+					</option>
+				))}
+			</select>
+			<div className="mr-6 flex justify-center items-center">
+				<div className="flex flex-col ">
+					<span className="font-bold">{user.name} </span>
+					<span className="text-[#cccccc]">{user.email}</span>
+				</div>
+				<span className="circle ml-3"></span>
+			</div>
 		</div>
-	</div>;
+	);
 };
--- src/core/constants/user.const.ts
+++ src/core/constants/user.const.ts
@@ -1,28 +1,31 @@
-
 export interface User {
-    name: string;
-    password: string;
-    ownedShops: number[]; // id
-    currentShop: number; // id
+	name: string;
+	password: string;
+	email: string;
+	ownedShops: number[]; // id
+	currentShop: number; // id
 }
 
 export const users: User[] = [
-    {
-        name: "niezalogowany",
-        password: "niezalogowany",
-        ownedShops: [],
-        currentShop: -1
-    },
-    {
-        name: "piotrek",
-        password: "piotrek",
-        ownedShops: [1, 2, 3],
-        currentShop: 1
-    },
-    {
-        name: "dawid",
-        password: "dawid",
-        ownedShops: [2, 3, 5],
-        currentShop: 2
-    },
-]
+	{
+		name: "niezalogowany",
+		password: "niezalogowany",
+		email: "niezalogowany@mail.pl",
+		ownedShops: [],
+		currentShop: -1,
+	},
+	{
+		name: "piotrek",
+		password: "piotrek",
+		email: "piotrek@mail.pl",
+		ownedShops: [1, 2, 3],
+		currentShop: 1,
+	},
+	{
+		name: "dawid",
+		password: "dawid",
+		email: "dawid@mail.pl",
+		ownedShops: [2, 3, 5],
+		currentShop: 2,
+	},
+];
