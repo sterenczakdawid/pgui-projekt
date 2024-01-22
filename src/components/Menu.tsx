0b61afdcce4e60690aec6bd7055b268381f9ed20
Dawid <sterenczak.dawid@gmail.com>
2024-01-22 23:18:44 +0100

    fixes

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,6 +5,12 @@ import { changeLang, changeTheme } from "../core/store/appSettingsSlice";
 import { NavLink } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { BsXLg } from "react-icons/bs";
+import { BsCardChecklist } from "react-icons/bs";
+import { BsJournalBookmark } from "react-icons/bs";
+import { BsHouse } from "react-icons/bs";
+import { BsBookmarkStar } from "react-icons/bs";
+import { BsStarHalf } from "react-icons/bs";
+import { BsLock } from "react-icons/bs";
 
 export const Menu = ({ showSidebar, toggleSidebar }) => {
 	const [activeThemeButton, setActiveThemeButton] = useState("light");
@@ -58,70 +64,71 @@ export const Menu = ({ showSidebar, toggleSidebar }) => {
 				{t("Dashboard")}
 			</h1>
 
-			<div className="flex flex-col items-center justify-center w-full my-5 text-sm">
+			<div className="flex flex-col items-center justify-center w-full my-5 text-[1rem]">
 				<NavLink
 					to="/"
-					className="text-white h-[72px] leading-[72px] w-[70%] text-left">
+					className="text-white h-[72px] leading-[72px] w-[70%] flex justify-left items-center">
+					{" "}
+					<BsHouse className="text-xl mx-2" />
 					{t("Dashboard")}
 				</NavLink>
 				<NavLink
 					to="/orders"
-					className="text-white h-[72px] leading-[72px] w-[70%] text-left">
+					className="text-white h-[72px] leading-[72px] w-[70%] flex justify-left items-center">
+					<BsCardChecklist className="text-xl mx-2" />
 					{t("Orders")}
 				</NavLink>
 				<NavLink
 					to="/sales-quality"
-					className="text-white h-[72px] leading-[72px] w-[70%] text-left">
+					className="text-white h-[72px] leading-[72px] w-[70%] flex justify-left items-center">
+					<BsBookmarkStar className="text-xl mx-2" />
 					{t("Quality")}
 				</NavLink>
 				<NavLink
 					to="/customer-reviews"
-					className="text-white h-[72px] leading-[72px] w-[70%] text-left">
+					className="text-white h-[72px] leading-[72px] w-[70%] flex justify-left items-center">
+					<BsStarHalf className="text-xl mx-2" />
 					{t("Opinions")}
 				</NavLink>
 				<NavLink
 					to="/sales-advice"
-					className="text-white h-[72px] leading-[72px] w-[70%] text-left">
+					className="text-white h-[72px] leading-[72px] w-[70%] flex justify-left items-center">
+					<BsJournalBookmark className="text-xl mx-2" />
 					{t("Advice")}
 				</NavLink>
 			</div>
-			<div>
+			<button className="text-white h-[72px] leading-[72px] w-full flex items-center justify-center absolute bottom-[60px]">
+				<BsLock className="text-xl mx-2" />
+				{t("Logout")}
+			</button>
+			<div className="absolute bottom-0 left-[10%]">
 				<button
 					className={`left ranking__button border-white ${
 						activeThemeButton === "dark" ? "active__theme__button" : ""
 					}`}
 					onClick={() => handleThemeChange("dark")}>
-					🌑
+					<span className="block w-[20px] h-[22px]">🌑</span>
 				</button>
 				<button
 					className={`right ranking__button border-white ${
 						activeThemeButton === "light" ? "active__theme__button" : ""
 					}`}
 					onClick={() => handleThemeChange("light")}>
-					☀️
+					<span className="block w-[20px] h-[22px]">☀️</span>
 				</button>
 			</div>
-			<h2>{theme}</h2>
-			<div>
+			<div className="absolute bottom-0 right-[10%] text-[30px]">
 				<button
-					className={`left ranking__button border-white ${
-						activeLangButton === "en" ? "active__theme__button" : ""
-					}`}
+					className={`left ranking__button border-white  ${theme === "dark" ? activeLangButton === 'en' ? 'active__theme__button' : "text-[#353535]" : activeLangButton === 'pl' ? 'active__theme__button' : ""}`}
 					onClick={() => changeLanguage("en")}>
-					🇬🇧
+					<span className="block w-[20px] h-[22px]">EN</span>
 				</button>
 				<button
-					className={`right ranking__button border-white ${
-						activeLangButton === "pl" ? "active__theme__button" : ""
-					}`}
+					className={`right ranking__button border-white ${theme === "dark" ? activeLangButton === 'pl' ? 'active__theme__button' : "text-[#353535]" : activeLangButton === 'en' ? 'active__theme__button' : ""}`}
 					onClick={() => changeLanguage("pl")}>
-					🇵🇱
+					<span className="block w-[20px] h-[22px]">PL</span>
 				</button>
 			</div>
-			{/* <button onClick={() => dispatch(changeTheme())}>Change theme</button> */}
-			<button className="text-white text-center h-[72px] leading-[72px] w-full">
-				{t("Logout")}
-			</button>
 		</div>
 	);
 };
